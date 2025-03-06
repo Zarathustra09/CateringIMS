5708f93312a00245b1749719a8afc69c238ba316
Zarathustra09 <joshua.pardo30@gmail.com>
2025-03-06 09:38:57 +0800

    March 06 2025 - better calendar design

--- resources/views/admin/reservationitems/index.blade.php
+++ resources/views/admin/reservationitems/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container-xxl flex-grow-1 container-p-y">
         <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center">
                 <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Home /</span> Reservation</h4>
