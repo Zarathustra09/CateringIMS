91e40f7b3881b8b95bfc0b582d1cd0c1a7b5f777
Zarathustra09 <joshua.pardo30@gmail.com>
2025-03-19 12:00:00 +0800

    March 19 2025 - updated hero section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,24 +6,19 @@
 <main class="main">
 
     <!-- Hero Section -->
-    <section id="hero" class="hero section light-background">
-
+    <section id="hero" class="hero section" style="background-image: url('{{asset('landingpage/assets/img/hero-img.jpg')}}');">
         <div class="container">
-            <div class="row gy-4 justify-content-center justify-content-lg-between">
-                <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <div class="row gy-5 justify-content-center align-items-center">
+                <div class="col-lg-8 col-xl-6 d-flex flex-column justify-content-center">
                     <h1 data-aos="fade-up">Delicious Moments Served:<br>Your Perfect Catering Awaits!</h1>
-                    <p data-aos="fade-up" data-aos-delay="100">Elevate your events with our seamless catering reservations, where every bite is a celebration!</p>
+                    <p data-aos="fade-up" data-aos-delay="100">Elevate your events with our seamless catering reservations, where every bite is a celebration of flavor and elegance!</p>
                     <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-                        <a href="#book-a-table" class="btn-get-started">Reserve Now!</a>
-                        {{-- <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
+                        <a href="{{route('login')}}" class="btn-get-started">Reserve Now!</a>
+{{--                        <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>--}}
                     </div>
-                </div>
-                <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
-                    <img src="{{asset('landingpage/assets/img/hero-img.jpg')}}" class="img-fluid animated" alt="">
                 </div>
             </div>
         </div>
-
     </section><!-- /Hero Section -->
 
     <!-- About Section -->
