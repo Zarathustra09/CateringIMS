6e977275e58217f73ecd118c81ed4197e67c80dd
Zarathustra09 <joshua.pardo30@gmail.com>
2025-03-24 11:18:03 +0800

    March 24 2025 - added 2025 in the footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -127,7 +127,7 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-        <p>© <span>Copyright</span> <strong class="px-1 sitename">Jhulian's Suite</strong> <span>All Rights Reserved</span></p>
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Jhulian's Suite</strong> <span>All Rights Reserved 2025</span></p>
         <div class="credits">
             <!-- Licensing information -->
         </div>
