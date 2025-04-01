390db22f613a0f535e5a6328aebe6315187a5e6a
Zarathustra09 <joshua.pardo30@gmail.com>
2025-04-01 15:48:46 +0800

    April 1 2025 - updated header functionalities in my staff.header

--- resources/views/layouts/staff/header.blade.php
+++ resources/views/layouts/staff/header.blade.php
@@ -1,7 +1,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="javascript:void(0);" class="app-brand-link">
-            <img src="{{asset('landingpage/assets/img/logoname.jpg')}}" class="img-fluid" alt="" style="max-height: 60px;">
+            <img src="{{asset('landingpage/assets/img/logoname.jpg')}}" class="img-fluid" alt="" style="max-height: 70px;">
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
@@ -11,29 +11,26 @@
 
     <div class="menu-inner-shadow"></div>
 
-    <ul class="menu-inner py-1">
+    <ul class="menu-inner py-4">
         <!-- Dashboard -->
-        <li class="menu-item {{ request()->routeIs('home') ? 'active' : '' }}">
+        <li class="menu-item {{ request()->routeIs('staff.home') ? 'active' : '' }}">
             <a href="{{route('staff.home')}}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-alt"></i>
                 <div data-i18n="Home">Home</div>
             </a>
         </li>
 
-
-        <!-- Catering Management -->
+        <!-- Reservation Management -->
         <li class="menu-header small text-uppercase"><span class="menu-header-text">Reservation Management</span></li>
-
-
-        <li class="menu-item {{ Request()->routeIs('staff/reservations') ? 'active' : '' }}">
+        <li class="menu-item {{ Request()->routeIs('staff.staffreservation*') ? 'active' : '' }}">
             <a href="{{ route('staff.staffreservation.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-list-ul"></i>
                 <div data-i18n="Reservations">Reservations</div>
             </a>
         </li>
 
+        <!-- Payroll Management -->
         <li class="menu-header small text-uppercase"><span class="menu-header-text">Payroll Management</span></li>
-
         <li class="menu-item {{ request()->routeIs('staff.payroll.index') ? 'active' : '' }}">
             <a href="{{ route('staff.payroll.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-dollar-circle"></i>
@@ -41,14 +38,13 @@
             </a>
         </li>
 
+        <!-- Employee Management -->
         <li class="menu-header small text-uppercase"><span class="menu-header-text">Employee Management</span></li>
-
         <li class="menu-item {{ request()->routeIs('staff.staffdetail.index') ? 'active' : '' }}">
             <a href="{{ route('staff.staffdetail.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-user"></i>
                 <div data-i18n="StaffDetail">My Detail</div>
             </a>
         </li>
-
     </ul>
 </aside>
